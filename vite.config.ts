cfc17bfb40085d9724e135106529264d1c0959dc
Kamesh <33164417+KameshMurali@users.noreply.github.com>
2025-05-02 23:47:47 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/KameshMurali/ShootK/', // Replace this with your actual repository name
+  base: '/ShootK/', // Replace this with your actual repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
